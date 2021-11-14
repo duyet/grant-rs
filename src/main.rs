0de01fa49c318da6aa2a7f3e6c029cce75c48bfc
Duyet Le <lvduyet@fossil.com>
2021-11-14 17:34:48 +0700

    chore: update cli

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,42 @@ struct CustomError(String);
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Debug, StructOpt)]
 struct Cli {
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: PathBuf,
-    /// Dry run
-    #[structopt(short, long)]
-    dryrun: bool,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// Generate project
+    Gen {
+        /// The target folder
+        #[structopt(short, long, default_value = ".")]
+        target: String,
+    },
+
+    /// Apply changes
+    Apply {
+        /// The path to the file to read
+        #[structopt(short, long, parse(from_os_str))]
+        file: PathBuf,
+
+        /// Dry run
+        #[structopt(short, long)]
+        dryrun: bool,
+    },
 }
 
 fn main() -> Result<()> {
-    let args = Cli::from_args();
+    match Cli::from_args().cmd {
+        Command::Gen { target } => {
+            println!("Generated to {}", target);
+        },
 
-    println!("Debug: params = {:?}", args);
+        Command::Apply { file, dryrun } => {
+            println!("Applying from {:?}, dry-run = {}", file, dryrun);
+        },
 
-    let content = grant::get_content(&args.path)?;
-    println!("file content: {}", content);
+    }
 
     Ok(())
 }
