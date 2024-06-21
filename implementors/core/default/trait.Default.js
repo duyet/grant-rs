ad13691b4c6a59611927a8d1da77534d3c79c2af
duyet <duyet@users.noreply.github.com>
2024-06-21 11:49:26 +0000

    deploy: a4dc62598e315285f233220cf6e1152cad3f7dfd

--- implementors/core/default/trait.Default.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"grant":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"grant/config/connection/struct.Connection.html\" title=\"struct grant::config::connection::Connection\">Connection</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"grant/config/config_base/struct.Config.html\" title=\"struct grant::config::config_base::Config\">Config</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"grant":["",[["config",[],["config_base.rs","connection.rs","mod.rs","role.rs","role_database.rs","role_schema.rs","role_table.rs","user.rs"]]],["apply.rs","cli.rs","connection.rs","gen.rs","inspect.rs","lib.rs","validate.rs"]]\
-}');
-createSourceSidebar();
